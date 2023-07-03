f400bd07238d741ce0fc04464bd64f686d2c28dc
matheos_djidonou <teckmathd@gmail.com>
2023-07-03 06:28:18 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - main entry for the programm
  * Return: 1
@@ -6,6 +7,6 @@
 int main(void)
 {
  fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-    return (1);
+ return (1);
 }
 
