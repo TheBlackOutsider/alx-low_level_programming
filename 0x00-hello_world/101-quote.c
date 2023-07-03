791eee051341e851ed4a9b660702cdcbcd142608
matheos_djidonou <teckmathd@gmail.com>
2023-07-03 06:48:20 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,8 @@
  */
 int main(void)
 {
-fwrite(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+fwrite(message, sizeof(char), strlen(message), stderr);
 return (1);
 }
 
