2c476546ceafd46cfb1402143aef950b075e1d81
matheos_djidonou <teckmathd@gmail.com>
2023-07-04 13:40:38 -0700

    Finalize the project of pointers,arrays,strings

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- * reset_to_98 -  takes a pointer to an int as parameter 
+ * reset_to_98 -  takes a pointer to an int as parameter
  * and updates the value it points to to 98
  * @n: parameter on pointer
  * Return: void
 */
-
 void reset_to_98(int *n)
 {
 
