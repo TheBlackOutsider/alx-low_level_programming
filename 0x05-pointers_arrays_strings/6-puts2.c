60f20c265a139b6f415fdfdc737ff25f5ece6f7c
matheos_djidonou <teckmathd@gmail.com>
2023-07-04 13:42:34 -0700

    Finalize the project of pointers,arrays,strings

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,16 +8,16 @@
 */
 void puts2(char *str)
 {
-int i = 0;
-while (str[i] != '\0')
-{
-	if (i % 2 == 0)
-        {
-            _putchar(str[i]);
-        }
+	int i = 0;
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+        	{
+            		_putchar(str[i]);
+        	}
 
-        i++;
-}
-_putchar('\n');
+        	i++;
+	}
+	_putchar('\n');
 }
 
